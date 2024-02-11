ec6e47e1643bd49e9bac91af2ae726f383b4489e
w33zl <7383510+w33zl@users.noreply.github.com>
2024-02-11 19:07:06 +0100

    Added base scriptLibrary main file

---
+++ scripts/scriptLibrary/scriptLibrary.lua
@@ -0,0 +1,10 @@
+_G.g_scriptLibraryDir = g_currentModDirectory .. "scripts/scriptLibrary/"
+
+-- Generic libraries
+source(g_scriptLibraryDir .. "ModHelper.lua")
+
+-- Specializations
+-- source(g_scriptLibraryDir .. "")
+
+-- Base Game Class Extensions
+-- source(g_scriptLibraryDir .. "")
