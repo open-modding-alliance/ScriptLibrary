568e44912b6c53a90b019d76259a6459641871cf
w33zl <7383510+w33zl@users.noreply.github.com>
2024-07-01 20:26:25 +0200

    Cleanup scriptLibrary base script

--- scripts/scriptLibrary/scriptLibrary.lua
+++ scripts/scriptLibrary/scriptLibrary.lua
@@ -1,9 +1,34 @@
+--[[
+
+FS Script Library - A collection of utility/helper classes for Farming Simulator mods
+
+Author(s):  Open Modding Alliance (w33zl)
+Version:    1.0.0
+
+License:    CC BY-NC-SA 4.0
+This license allows reusers to distribute, remix, adapt, and build upon the material in any medium or 
+format for noncommercial purposes only, and only so long as attribution is given to the creator.
+If you remix, adapt, or build upon the material, you must license the modified material under identical terms. 
+
+
+USAGE:
+
+    Add the following to your modDesc.xml file:
+    <extraSourceFiles>
+        <sourceFile filename="scripts/scriptLibrary/scriptLibrary.lua" />
+    </extraSourceFiles>`
+
+]]
+
 _G.g_scriptLibraryDir = g_currentModDirectory .. "scripts/scriptLibrary/"
 
--- Generic libraries
+-- ### Core libraries (recommended) ###
 source(g_scriptLibraryDir .. "ModHelper.lua") -- Recommended for all mods, enables the Mod:init() wrapper to create the mod object
 
--- Specializations
+-- ### Additional libraries (optional) ###
+
+
+-- *** Specializations ***
 -- source(g_scriptLibraryDir .. "")
 
 -- Base Game Class Extensions
